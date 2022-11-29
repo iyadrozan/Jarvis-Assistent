52060752edf84cd4d748ca73300eae250920fa4e
iyadrozan <iyadrozan26@gmail.com>
2022-11-29 22:15:10 +0800

    add google search command

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ import pyttsx3
 import datetime
 import speech_recognition as sr
 import wikipedia
+import pyfiglet
+from selenium import webdriver
 
 engine = pyttsx3.init()
 
@@ -10,7 +12,7 @@ def speak(audio):
     en_voice_id = "english-us"
     id_voice_id = "indonesian"
     voices = engine.getProperty('voices')
-    engine.setProperty('rate', 126)
+    engine.setProperty('rate', 129)
     for voice in voices:
         voices = engine.setProperty('voice', en_voice_id)
     engine.runAndWait()
@@ -31,21 +33,32 @@ def date():
     speak(year)
     print(year, month.strftime("%B"), day.strftime("%A"))
 
+def searchGoogle(link):
+    driver = webdriver.Firefox()
+    speak("open the Firefox")
+    driver.get("https://www.google.com/")
+    searchInput = driver.find_element("class name", "gLFyf")
+    searchInput.send_keys(link)
+    searchInput.submit()
+    speak("This is what i found")
+
 def wishme():
-    speak("Welcome back sir!")
+    ascii_banner = pyfiglet.figlet_format("JARVIS Assistent")
+    print(ascii_banner)
     hour = datetime.datetime.now().hour
     if hour >= 6 and hour<12:
         speak("Good Morning Sir!")
-        print("Morning")
+        print("Good Morning")
     elif hour >=12 and hour<18:
         speak("Good afternoon. Sir")
-        print("Afternoon")
+        print("Good Afternoon")
     elif hour >= 18 and hour<24:
         speak("Good Evening Sir!")
-        print("Evening")
+        print("Good Evening")
     else:
         speak("Good Night Sir!")
-        print("Night")
+        print("Good Night")
+    speak("Welcome back")
     speak("can i help you sir?")
 
 def takeCommand():
@@ -68,6 +81,7 @@ def takeCommand():
         return "None"
     return query
 
+
 if __name__ == "__main__":
     wishme()
     while True:
@@ -79,13 +93,23 @@ if __name__ == "__main__":
             date()
         elif 'are you jarvis' in query:
             speak("yes sir, i am here for you")
+        elif 'who are you' in query:
+            speak("i am a jarvis sir!")
         elif 'who am i' in query:
-            speak("Iyad Rozan My BOSS")
-        elif 'wikipedia' in query:
+            speak("You Are Iyad Rozan")
+
+        elif 'search in google' in query:
+            speak("What should i search?")
+            link = takeCommand().lower()
+            print(link)
+            searchGoogle(link)
+
+        elif 'search on wikipedia' in query:
             speak("Searching...")
-            query = query.replace("wikipedia", "")
+            query = query.replace("search on wikipedia", "")
             result = wikipedia.summary(query, sentences=2)
             print(result)
             speak(result)
+
         elif 'offline' in query:
             quit()
